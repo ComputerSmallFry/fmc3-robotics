2f44d9a84d7f74a341b2f071d60fbf64ba4ebdd5
Haoan Wang <wanghaoan.victor@gmail.com>
2025-11-01 15:12:58 +0100

    haoanw: add shell scripts for demo

---
+++ projects/lerobot/demo_scripts/lerobot_replay_dataset.sh
@@ -0,0 +1,8 @@
+lerobot-replay \
+    --robot.type=so101_follower \
+    --robot.port=/dev/ttyACM1 \
+    --robot.id=follower_arm \
+    --dataset.root=./record_dataset \
+    --dataset.repo_id=local/dataset_01 \
+    --dataset.episode=0 \
+    
